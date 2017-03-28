c0de02802a438f5137aa1c3b5be049a62cd3aa3d
EnderRL <carlosroldanmontaner@gmail.com>
2017-03-28 18:41:36 +0200

    AmlTrack Class Skeleton

--- src/music/AmlTrack.java
+++ src/music/AmlTrack.java
@@ -1,7 +1,50 @@
 package music;
 
-/**
- * Created by juanm on 28/03/2017.
- */
+import javax.sound.midi.Track;
+
 public class AmlTrack {
+
+    private int currentTick;
+    private int[] metric;
+    private int tone;
+    private Track track;
+
+    public AmlTrack(Track track) {
+
+    }
+
+    public Track getTrack() {
+        return track;
+    }
+
+    public void setTrack(Track track) {
+        this.track = track;
+    }
+
+    public int getCurrentTick() {
+        return currentTick;
+    }
+
+    public void setCurrentTick(int currentTick) {
+        this.currentTick = currentTick;
+    }
+
+    public int[] getMetric() {
+        return metric;
+    }
+
+    public void setMetric(int[] metric) {
+        this.metric = metric;
+    }
+
+    public int getTone() {
+        return tone;
+    }
+
+    public void setTone(int tone) {
+        this.tone = tone;
+    }
+
+
+
 }
