0101631da44bb91830cb89a959c0cca4b535f06c
EnderRL <carlosroldanmontaner@gmail.com>
2017-03-28 18:52:08 +0200

    AmlTrack Class Skeleton v2

--- src/music/AmlTrack.java
+++ src/music/AmlTrack.java
@@ -1,5 +1,6 @@
 package music;
 
+import javax.sound.midi.MidiEvent;
 import javax.sound.midi.Track;
 
 public class AmlTrack {
@@ -9,8 +10,19 @@ public class AmlTrack {
     private int tone;
     private Track track;
 
-    public AmlTrack(Track track) {
+    public AmlTrack(Track track, int[] metric, int tone) {
+        this.track = track;
+        this.metric = metric;
+        this.tone = tone;
+        currentTick = 0;
+    }
 
+    public void addCompas(AmlCompas compas) {
+        for(AmlNote note : compas.getNotes()){
+            track.add(new MidiEvent(note.getOnMessage(), currentTick));
+            currentTick += note.getDuration();
+            track.add(new MidiEvent(note.getOffMessage(), currentTick));
+        }
     }
 
     public Track getTrack() {
