2537dead8e79dcc30bc3af5c8f2ec5116d0b0fdc
Daryan7 <juanmiguel1996@gmail.com>
2017-05-25 14:28:53 +0200

    Prepared next implementation

--- src/data_structures/ChannelManager.java
+++ src/data_structures/ChannelManager.java
@@ -9,16 +9,18 @@ import java.util.ListIterator;
 public class ChannelManager {
 
     private ArrayList<LinkedList<IntervalTrack>> channels;
+    private LinkedList<IntervalTrack> tracks;
 
     public ChannelManager() {
         channels = new ArrayList<>(16);
         for (int i = 0; i < 16; ++i) channels.add(new LinkedList<>());
+        tracks = new LinkedList<>();
     }
 
     private static int nextChannel(int channel) {
         return channel == 8 ? channel+2 : channel+1;
     }
-    
+
     public void addTrack(int channel, IntervalTrack node) {
         ListIterator<IntervalTrack> iterator = channels.get(channel).listIterator();
         while(iterator.hasNext()) {
@@ -38,10 +40,22 @@ public class ChannelManager {
                 }
             }
             else if (node.end <= child.start) {
+                ListIterator<IntervalTrack> tracksIterator = tracks.listIterator();
+                while (tracksIterator.hasNext()) {
+                    IntervalTrack interval = tracksIterator.next();
+                    if (node.end <= interval.start) break;
+                }
+                tracksIterator.add(node);
                 iterator.add(node);
                 return;
             }
         }
+        ListIterator<IntervalTrack> tracksIterator = tracks.listIterator();
+        while (tracksIterator.hasNext()) {
+            IntervalTrack interval = tracksIterator.next();
+            if (node.end <= interval.start) break;
+        }
+        tracksIterator.add(node);
         iterator.add(node);
     }
 }
