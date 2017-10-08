c9e7156b1af85585b6225b05782203140e001340
Mario <mario_bcn2@hotmail.es>
2017-10-08 14:32:23 +0200

    Revision

--- G_11_AML/src/exceptions/AmlSemanticException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class AmlSemanticException extends AmlException {
-    public AmlSemanticException(String message, int line) {
-        super(message);
-        this.line = line;
-    }
-}
